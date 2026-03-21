3e66264068684a2257ec9503e6ffd7b2749dbd71
Jesus Moreno <jrafael.mv26@gmail.com>
2026-03-21 10:55:49 -0500

    first commit

---
+++ src/main/java/com/jesus/springboot/app/srpingboot_crud/SrpingbootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.jesus.springboot.app.srpingboot_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingbootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingbootCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesus/springboot/app/srpingboot_crud/SrpingbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus.springboot.app.srpingboot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
